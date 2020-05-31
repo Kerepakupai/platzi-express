20122e04b0cd011260ff2cc66f87a83f02179288
David Fuentes <kerepakupai@outlook.com>
2020-05-31 19:23:34 -0400

    ecommerce with pug and express

---
+++ ecommerce/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const productsRouter = require("./routes/products");
+const app = express();
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use("/products", productsRouter);
+
+const server = app.listen(8000, () => {
+  console.log(`Escuchando en: http://localhost:${server.address().port}`);
+});
